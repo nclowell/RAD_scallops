ef89240e5c9ccdc98e9e50891b819003242fd14e
nclowell <nclowell@uw.edu>
2017-03-13 16:47:07 -0700

    added prelim dapc plot, updated notebook

--- CRAGIG_run1/Scripts/CRAGIG_RUN1_DAPC_20170313.R
+++ CRAGIG_run1/Scripts/CRAGIG_RUN1_DAPC_20170313.R
@@ -5,24 +5,35 @@ data_all_loci <-read.genepop("doublet.gen")
 names(data_all_loci)
 data_all_loci$pop
 
-WA_Strait <- rep("WA1",26)
-WA_SanJuans <- rep("WA2",15)
 WA_Dabob <- rep("WA3", 11)
+WA_SanJuans <- rep("WA2",14) # missing one for some reason! it's not even in the first genepop prior to filtering...
 AK <- rep("AK1",7)
 CA_Catalina_06seg <- rep("CA1",12) # these add to 71!
+WA_Strait <- rep("WA1",26)
 
 
-pop_groups <- as.factor(c(rep("WA1",26),rep("WA2",15),rep("WA3", 11),rep("AK1",7),rep("CA1",12)))
+
+
+
+
+pop_groups <- as.factor(c(rep("WA3", 11),rep("WA2",14),rep("AK1",7),rep("CA1",12),rep("WA1",26)))
                         
-pop_labels <- c(WA_Strait,WA_SanJuans,WA_Dabob,AK,CA_Catalina)
+pop_labels <- c(WA_Dabob,WA_SanJuans,AK,CA_Catalina,WA_Strait)
 
 pop_cols <- c("black","dodgerblue","tomato","deepskyblue","red")
 
-dapc_all <- dapc(data_all_loci,data_all_loci$pop,n.pca=465,n.da=8) ##Retain all, then identify optimal number by optim.a.score
+
+#Data exploration to find # PCs. Go through all of their stoopid prompts.
+dapc_all <- dapc(data_all_loci,data_all_loci$pop)
+# retain N/3 Prinicpal components
+# Look at histogram; retain given number of linear discriminants (I think that is == n.da)
+myclusters <- find.clusters(data_all_loci)
 test_a_score <- optim.a.score(dapc_all)
-dapc_all <- dapc(data_all_loci,data_all_loci$pop,n.pca=25,n.da=8) ##63 PC's is the optimal number
+
+
+dapc_all <- dapc(data_all_loci,data_all_loci$pop,n.pca=22,n.da=4) ##22 PC's is the optimal number
 
 #2D plot
 scatter(dapc_all,scree.da=FALSE,cellipse=0,leg=FALSE,label=c("WA1", "WA2","WA3","AK1","CA1"),
         posi.da="bottomleft",csub=2,col=pop_cols,cex=1.5,clabel=1,pch=c(18,15,15,16,16),solid=1)
-legend(x = -4, y = 2,bty='n',legend=c("WA1", "WA2","WA3","AK1","CA1"),pch=c(18,15,15,16,16),col=pop_cols,cex=1.3)
+legend(x = -5, y = 2,bty='n',legend=c("WA1", "WA2","WA3","AK1","CA1"),pch=c(18,15,15,16,16),col=pop_cols,cex=1.3)
