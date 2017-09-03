f1fdb37eeb46fa53b9c606f0e98366aba0641463
nclowell <nclowell@uw.edu>
2017-09-03 11:53:49 -0700

    updated NBs, added script for pop ID to str files

--- CRAGIG_combined_lanes/Scripts/add_popIDs_to_str_files.py
+++ CRAGIG_combined_lanes/Scripts/add_popIDs_to_str_files.py
@@ -18,3 +18,34 @@ parser.add_argument("-i", "--infile", help="input structure file that needs popu
 parser.add_argument("-p", "--popfile", help="population assignment file from ipyrad", type=str, required=True)
 parser.add_argument("-o", "--outfile", help="new structure file with population numbers in second column", type=str, required=True)
 args = parser.parse_args()
+
+# get individual-population data into a dictionary from pop assignment file
+pop_dict = {}
+popfile = open(args.popfile, "r")
+for line in popfile:
+    if line != "":
+        linelist = line.strip().split()
+        individual = linelist[0]
+        population = linelist[1][3:]
+        pop_dict[individual] = population
+popfile.close()
+
+# add population ID number to rows in structure file
+outstrfile = open(args.outfile, "w")
+
+instrfile = open(args.infile, "r")
+instrfile_lines = instrfile.readlines()
+
+outstrfile.write(instrfile_lines[0]) # write old header to new structure file
+
+for line in instrfile_lines[1:]:
+    linelist = line.strip().split()
+    newline = linelist[0] + "\t" + str(pop_dict[linelist[0]]) + "\t"
+    for thing in linelist[2:]:
+        newline += thing + " "
+    newline = newline[:-1]
+    newline += "\n"
+    outstrfile.write(newline)
+
+outstrfile.close()
+instrfile.close()
